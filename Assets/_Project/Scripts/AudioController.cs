ab80c6ee9a8373e08828759795b14d36053c4bf0
Ferhat Eren Dalçık <f.dalcik@azerion.com>
2022-11-28 12:34:42 +0300

    add first spectrum control and visuals

--- Assets/_Project/Scripts/AudioController.cs
+++ Assets/_Project/Scripts/AudioController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private ParticleSystem _soundObject;
+    [SerializeField] private ParticleSystem _soundObject2;
+    [SerializeField] private ParticleSystem _soundObject3;
+    [SerializeField] private AudioSource _audioSource;
+    
+    private float[] _spectrumData;
+ 
     void Start()
     {
-        
+        // Array of all spectrum data will be saved
+        _spectrumData = new float[256];
+        StartCoroutine(StartDelayed());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        AudioListener.GetSpectrumData(_spectrumData, 0, FFTWindow.Rectangular);
         
+        for (int i = 0; i < _spectrumData.Length ; i++)
+        {
+            float tmp = _spectrumData[i] * 10;
+            if (tmp > 2F)
+            {
+                ChangeParticleEmission(tmp);
+            }
+        }
+    }
+
+    private ParticleSystem.EmissionModule _particleEmission;
+    private ParticleSystem.MainModule main;
+    private void ChangeParticleEmission(float changeRate)
+    {
+        _particleEmission = _soundObject.emission;
+        _particleEmission.rateOverTime = changeRate;
+
+        main = _soundObject2.main;
+        main.startSize = changeRate;
+    }
+
+    private IEnumerator StartDelayed()
+    {
+        yield return new WaitForSeconds(3F);
+        _audioSource.Play();
     }
 }
